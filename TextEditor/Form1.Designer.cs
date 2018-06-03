3eaad4685216a968376caae946a14e3f30ff63b7
Albert Soczewski <albert.soczewski@gmail.com>
2018-06-03 17:53:44 +0200

    Add project files.

---
+++ TextEditor/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textArea = new System.Windows.Forms.RichTextBox();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // textArea
+            // 
+            this.textArea.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textArea.Location = new System.Drawing.Point(0, 24);
+            this.textArea.Name = "textArea";
+            this.textArea.Size = new System.Drawing.Size(800, 426);
+            this.textArea.TabIndex = 0;
+            this.textArea.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textArea);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox textArea;
+    }
+}
+
---
+++ TextEditor/Levenshtein.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace TextEditor
+{
+    public class Levenshtein
+    {
+        public int CalculateLevenshteinDistance(string text, string pattern)
+        {
+            if (text == null && pattern == null)
+                return 0;
+            if (string.IsNullOrEmpty(text))
+                return pattern.Length;
+            if (string.IsNullOrEmpty(pattern))
+                return text.Length;
+
+            var tempResults = new int[text.Length + 1, pattern.Length + 1];
+            for (short i = 0; i < text.Length + 1; i++)
+            {
+                for (short j = 0; j < pattern.Length + 1; j++)
+                {
+                    if (i == 0)
+                        tempResults[i, j] = j;
+                    if (j == 0)
+                        tempResults[i, j] = i;
+                    if (i == 0 || j == 0)
+                        continue;
+
+                    var isSubstitution = char.ToLower(text.ElementAt(i - 1)) != char.ToLower(pattern.ElementAt(j - 1));
+                    var deletionCase = tempResults[i - 1, j] + 1;
+                    var insertionCase = tempResults[i, j - 1] + 1;
+                    var otherCase = tempResults[i - 1, j - 1] + (isSubstitution ? 1 : 0);
+                    tempResults[i, j] = new[] { deletionCase, insertionCase, otherCase }.Min();
+                }
+            }
+
+            return tempResults[text.Length, pattern.Length];
+        }
+    }
+}
